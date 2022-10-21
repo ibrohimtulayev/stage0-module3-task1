17d13e8baccb6246f1cb0ff3eb2ed3f6ed084f3a
tulayevibrohim <tulayevibrohimjon@gmail.com>
2022-10-21 20:21:22 +0500

    no

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -1,7 +1,9 @@
 package lang.print.gaps.task1;
 
+import static javax.swing.text.html.HTML.Tag.I;
+
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        System.out.println("\"I'm updated");
     }
 }
