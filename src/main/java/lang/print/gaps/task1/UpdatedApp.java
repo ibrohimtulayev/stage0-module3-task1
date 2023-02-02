658eb84e7dcdb85c87dcb69e258c9a0a485803b0
tulayevibrohim <tulayevibrohimjon@gmail.com>
2023-02-02 22:28:21 +0500

    hi

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -4,6 +4,6 @@ import static javax.swing.text.html.HTML.Tag.I;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("\"I'm updated");
+        System.out.println("I'm updated\ncan be compiled. And can write to console");
     }
 }
